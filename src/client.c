2e67e1a593ee4eb11cc6c73ab12b6e5f5b8bdc56
Saksham Gupta <sakshamgupta05@gmail.com>
2019-04-12 19:53:53 +0530

    added client bench mode

--- src/client.c
+++ src/client.c
@@ -1,7 +1,12 @@
+#define _POSIX_SOURCE
 #include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
 #include "chat.h"
 
-int main() {
+enum mode{manual, bench};
+
+void client(enum mode m, int M) {
   int sfd;
   struct sockaddr_in svaddr;
   char cmd[ARG_MAX];
@@ -27,15 +32,66 @@ int main() {
 
   pid_t pid = fork();
   if (pid == 0) {
-    while (fgets(cmd, ARG_MAX, stdin) != NULL) {
+    if (m == manual) {
+      while (fgets(cmd, ARG_MAX, stdin) != NULL) {
+        write(sfd, cmd, strlen(cmd));
+      }
+    } else {
+      strcpy(cmd, "JOIN name\n");
+      write(sfd, cmd, strlen(cmd));
+      strcpy(cmd, "BMSG hello\n");
+      for (int i = 0; i < M; i++) {
+        write(sfd, cmd, strlen(cmd));
+      }
+      strcpy(cmd, "LEAV\n");
       write(sfd, cmd, strlen(cmd));
     }
-
+    close(sfd);
+    exit(EXIT_SUCCESS);
   } else {
     while (readLine(sfd, cmd, ARG_MAX) > 0) {
-      printf("%s\n", cmd);
+      if (m == manual) {
+        printf("%s\n", cmd);
+      }
     }
     close(sfd);
     kill(pid, SIGKILL);
   }
+}
+
+void client_bench(int N, int M, int T) {
+  int n = 0;
+  for (int i = 0; i < T; i++) {
+    printf("client %d connected\n", i);
+    pid_t pid = fork();
+    if (pid == -1) {
+      perror("fork");
+      exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+      client(bench, M);
+      exit(EXIT_SUCCESS);
+    }
+
+    if (++n >= N) {
+      wait(NULL);
+      n--;
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    printf("starting in manual mode\n");
+    client(manual, 0);
+  } else if (argc == 4) {
+    printf("starting in benchmark mode\n");
+    int N = atoi(argv[1]);
+    int M = atoi(argv[2]);
+    int T = atoi(argv[3]);
+    client_bench(N, M, T);
+  } else {
+    printf("provide 0 args for manual mode or 3 args (N, M, T) for benchmark mode\n");
+    exit(EXIT_FAILURE);
+  }
+  return 0;
 }
